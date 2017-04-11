2c2dc1bd00b151529dbd76aa64ca649c6ed6ddf9
Mike <stavinski_2000@yahoo.com>
2017-04-11 13:08:13 +0100

    allowing user contents to be copied out from bin

--- chapter3/recycle_bin.py
+++ chapter3/recycle_bin.py
@@ -1,6 +1,8 @@
 import os
 import _winreg
 
+from argparse import ArgumentParser
+
 def recycle_bin_dir():
 	dirs = ["c:\\Recycler\\", "c:\\Recycled\\", "c:\\$Recycle.Bin\\"]
 	for dir in dirs:
@@ -19,14 +21,31 @@ def sid_to_username(sid):
 		return sid
 			
 def main():
+	parser = ArgumentParser("recycle bin util")
+
+	parser.add_argument("-s", "--show", action="store_true", help="show entries for all users")
+	parser.add_argument("-u", "--user", help="user to copy files for")
+	parser.add_argument("-o", "--output", help="output dir for copied files (default c:\\temp)", default="c:\\temp\\")
+	
+	args = parser.parse_args()
+	
 	search_dir = recycle_bin_dir()
 	print "[*] recycle bin: %s" % search_dir
-	for sid in os.listdir(search_dir):
-		files = os.listdir(os.path.join(search_dir, sid))
-		user = sid_to_username(sid)
-		print "[*] files for user: %s" % user
-		for file in files:
-			print "\t[+] %s" % file
+	
+	if args.show:
+		for sid in os.listdir(search_dir):
+			files = os.listdir(os.path.join(search_dir, sid))
+			user = sid_to_username(sid)
+			print "[*] files for user: %s" % user
+			for file in files:
+				print "\t[+] %s" % file
+	
+	if args.user is not None:
+		for sid in os.listdir(search_dir):
+			user = sid_to_username(sid)
+			if args.user == user:
+				print "[+] copying directory for user to %s" % args.output
+				os.system("XCOPY %s %s /Y /E /C /I /F /H" % (os.path.join(search_dir, sid), args.output))
 	
 if __name__ == "__main__":
 	main()
